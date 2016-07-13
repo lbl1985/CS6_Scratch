14d9ba810bc6d8689766afd029fe3e57bc39f47b
Binlong Li <herbert19lee@gmail.com>
2016-07-12 23:01:08 -0700

    first commit for cs_scratch.

---
+++ CS6_Scratch/HelloWorld.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class MainClass
+    {
+        static public void Main(string [] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ NewVariable/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewVariable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myAge = 31;
+            int x = 5;
+            double myHourlyRate = 150.00;
+            var myName = "Binlong Li";
+
+            Console.WriteLine($"myAge: {myAge}, x:{x}, myHourlyRate:{myHourlyRate}, myName:{myName}");
+
+            myAge = 35;
+            x = 20;
+            myHourlyRate = 85;
+            myName = "George Washington";
+            Console.WriteLine($"myAge: {myAge}, x:{x}, myHourlyRate:{myHourlyRate}, myName:{myName}");
+        }
+    }
+}
