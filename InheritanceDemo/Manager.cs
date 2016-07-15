09a643249152ed6fce0b83dbedead56895482366
Binlong Li <herbert19lee@gmail.com>
2016-07-14 23:43:22 -0700

    finish till Bugs and Exceptions.

---
+++ InheritanceDemo/Manager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceDemo
+{
+    public class Manager : Employee
+    {
+        public bool CompanyCar { get; set; }
+        public Manager(string name, bool hasCar) : base(name)
+        {
+            CompanyCar = hasCar;
+        }
+
+        public override void TakeVacation()
+        {
+            vacationDays += 15;
+        }
+
+        public override string ToString()
+        {
+            return $"[Manager. Name: {Name} hasCompanyCar: {CompanyCar}, Vaction: { vacationDays }];";
+        }
+
+        public void DriveCar()
+        {
+            if(CompanyCar)
+            {
+                Console.WriteLine("Driving Car");
+            }
+        }
+    }
+}
---
+++ TryCatch/Worker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatch
+{
+    public class Worker
+    {
+        public Worker()
+        {
+            Work();
+        }
+
+        public void Work()
+        {
+            Console.WriteLine("Open file here...");
+            try
+            {
+                Console.WriteLine("Throwing Exception...");
+                throw new System.Exception();
+                //Console.WriteLine("Closing File...");
+            }
+            catch
+            {
+                Console.WriteLine("Handling the exception");
+            }
+            finally
+            {
+                Console.WriteLine("Closing");
+            }
+        }
+    }
+}
