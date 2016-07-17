ea30d15432876769fca9cf03342da75afa1af1e1
Binlong Li <herbert19lee@gmail.com>
2016-07-17 00:17:52 -0700

    finish Interfaces module.

---
+++ AbstractClasses/Worker.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasses
+{
+    class Worker
+    {
+        public Worker()
+        {
+            work();
+        }
+
+        public void work()
+        {
+            List<Control> controls = new List<Control>();
+
+            Button button = new Button(0, 100, "Click the button");
+
+            ListBox listBox = new ListBox(100, 300, new List<string>());
+
+            Button cancelButton = new Button(150, 100, "cancel");
+
+            controls.Add(button);
+            controls.Add(listBox);
+            controls.Add(cancelButton);
+
+            foreach (Control control in controls)
+            {
+                control.Draw();
+            }
+
+        }
+    }
+}
---
+++ Interfaces/Worker.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public class Worker
+    {
+        public Worker()
+        {
+            work();
+        }
+
+        public void work()
+        {
+            Document doc1 = new Document();
+            doc1.Reformat();
+            doc1.Read("myFile");
+            doc1.Speak();
+
+            IStrorable doc2 = new Document();
+            doc2.Read("myFile 2");
+            doc2.Write("myFileWrite 2");
+
+
+            ISpeak doc3 = new Document();
+            doc3.Speak();
+
+            Document doc4 = new Memo();
+            doc4.Reformat();
+            doc4.Read("myFile 4");
+
+            Memo doc5 = new Memo();
+            doc5.Read("Myfile 5");
+            doc5.Reformat();
+            doc5.MemoTo = "Jesse";
+        }
+    }
+}
